e9edf9fbfee3add2bb7b7c892c35a2c94991bd4f
Breee <loeffler.uni@gmail.com>
2018-02-15 16:36:45 +0100

    configuration struct

--- config/Configuration.py
+++ config/Configuration.py
@@ -5,4 +5,34 @@ logger = logging.getLogger('discord')
 class Configuration(object):
 
     def __init__(self, config_file):
-        pass
+        self.token = ""
+        self.playing = ""
+        self.msg_update_interval = 100
+        self.msg_timeout = 0
+        self.read_config_file(config_file)
+
+    def read_config_file(self, config_file):
+        """
+        >>> Conf = Configuration("test.conf")
+        >>> Conf.token
+        'TESTTOKEN'
+        >>> Conf.playing
+        'TESTPLAYING'
+        >>> Conf.msg_update_interval
+        100
+        >>> Conf.msg_timeout
+        3600
+
+        :param config_file:
+        :return:
+        """
+        with open(config_file, 'r') as config:
+            for line in config:
+                if line.startswith("token="):
+                    self.token = line[line.find("=") + 1:].rstrip("\n")
+                if line.startswith("playing="):
+                    self.playing = line[line.find("=") + 1:].rstrip("\n")
+                if line.startswith("message_update_interval="):
+                    self.msg_update_interval = int(line[line.find("=") + 1:].rstrip("\n"))
+                if line.startswith("message_timeout="):
+                    self.msg_timeout = int(line[line.find("=") + 1:].rstrip("\n"))
