fcfd60d5de6973a7a8adf79c768b0915275697cb
Bree@Nox-vm <loeffler.uni@gmail.com>
2018-04-03 19:01:26 +0200

    finally added licence headers

--- pollbot/PollBot.py
+++ pollbot/PollBot.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2018 Breee@github
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from config.Configuration import Configuration
 from messages.MessageManager import MessageManager
 from storage.StorageManager import StorageManager
@@ -253,7 +277,10 @@ class PollBot(commands.Bot):
         poll_id = self.message_manager.pollmessage_id_to_poll_id[poll_message.id]
         # add reactions
         poll = self.poll_factory.polls[poll_id]
-        await self.delete_message(poll_message)
+        try:
+            await self.delete_message(poll_message)
+        except discord.NotFound:
+            LOGGER.info("Pollmessage for poll id #%d does not exist anymore" % poll_id)
 
         if post_notification:
             await self.send_message(trigger_message.channel,
